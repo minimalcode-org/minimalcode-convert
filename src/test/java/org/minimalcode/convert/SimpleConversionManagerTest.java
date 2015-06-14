3c904637f77401767dc349c0f8f5ef3e67f1a47b
Fabio Piro <pirofabio@gmail.com>
2015-06-14 14:08:33 +0200

    Fixed test for Jdk6

--- src/test/java/org/minimalcode/convert/SimpleConversionManagerTest.java
+++ src/test/java/org/minimalcode/convert/SimpleConversionManagerTest.java
@@ -1,12 +1,37 @@
 package org.minimalcode.convert;
 
 import org.junit.Test;
+import org.minimalcode.reflect.Bean;
+import org.minimalcode.reflect.Property;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import static org.junit.Assert.*;
 
 public class SimpleConversionManagerTest {
+
+    static class Book {
+        private List<String> titles;
+        private List<String> upperCasedTitles;
+
+        public List<String> getTitles() {
+            return titles;
+        }
+
+        public void setTitles(List<String> titles) {
+            this.titles = titles;
+        }
+
+        public List<String> getUpperCasedTitles() {
+            return upperCasedTitles;
+        }
+
+        public void setUpperCasedTitles(List<String> upperCasedTitles) {
+            this.upperCasedTitles = upperCasedTitles;
+        }
+    }
 
     @Test
     public void testAddConverter() {
@@ -14,14 +39,17 @@ public class SimpleConversionManagerTest {
     }
 
     @Test
+    public void testAddConverterResolved() {
+
+    }
+
+    @Test
     public void testCanConvertSimple() {
         SimpleConversionManager manager = new SimpleConversionManager();
-        assertFalse(manager.canConvert(String.class, Locale.class));
-
         manager.addConverter(new SimplePropertyConverter<String, Locale>() {
             @Override
             public Locale convert(String source) {
-                return Locale.forLanguageTag(source);
+                return new Locale(source);
             }
         });
 
@@ -33,7 +61,8 @@ public class SimpleConversionManagerTest {
 
     @Test
     public void testCanConvert() {
-
+        SimpleConversionManager manager = new SimpleConversionManager();
+        assertFalse(manager.canConvert(String.class, Locale.class));
     }
 
 
@@ -44,6 +73,29 @@ public class SimpleConversionManagerTest {
 
     @Test
     public void testConvert() {
+        SimpleConversionManager manager = new SimpleConversionManager();
+        manager.addConverter(List.class, List.class, new PropertyConverter<List<String>, List<String>>() {
+            @Override
+            public boolean canConvert(Property sourceProperty, Property targetProperty) {
+                return sourceProperty != null && sourceProperty.getActualType() == String.class
+                        && targetProperty != null && targetProperty.getActualType() == String.class;
+            }
+
+            @Override
+            public List<String> convert(List<String> source, Property sourceProperty, Property targetProperty) {
+                List<String> upperCasedTitles = new ArrayList<String>(source.size());
+
+                for (String title : source) {
+                    upperCasedTitles.add(title.toUpperCase());
+                }
+
+                return upperCasedTitles;
+            }
+        });
+
+        Bean<Book> bean = Bean.forClass(Book.class);
+        Property titles = bean.getProperty("titles");
+        Property upperCasedTitles = bean.getProperty("upperCasedTitles");
 
     }
 }
